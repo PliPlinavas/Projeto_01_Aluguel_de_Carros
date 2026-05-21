68f16d857e327b53775013a6b37f124ee0596ff2
André <andrescalcotoledo@gmail.com>
2026-05-21 16:26:03 -0300

    app2

--- app.py
+++ app.py
@@ -17,3 +17,32 @@ st.markdown('---')
 
 dias = st.text_input(f'Por quantos dias o {opcao} foi alugado?')
 km = st.text_input(f'quantos km você rodou com o {opcao}?')
+
+if opcao == 'GTR':
+    diaria = 440
+
+elif opcao == 'BMW':
+    diaria = 250
+
+elif opcao == 'Porsche':
+    diaria = 380
+
+elif opcao == 'Ferrari':
+    diaria = 700
+
+elif opcao == 'Maserati':
+    diaria = 500
+
+
+
+
+if st.button('Caucular'):
+    dias = int(dias)
+    km = float(km)
+
+    total_dias = dias * diaria
+    total_km = km * 0.15
+    aluguel_total = total_dias+total_km
+
+    st.warning(f'você alugou o {opcao} por {dias} dias e rodou {km}km. O valor total a pagar é R${aluguel_total:.2f}')
+
